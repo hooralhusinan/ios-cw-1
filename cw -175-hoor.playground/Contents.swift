eeb58ce157f4ab183cb349d3c0d6363570458350
hooralhusinan <66676227+hooralhusinan@users.noreply.github.com>
2020-06-13 17:05:44 +0300

    b

---
+++ cw -175-hoor.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var str = "Hello, playground"
+var name = "hoor"
+var hobby1 = "swimming"
+var hobby2 = "walkinhg"
+var age = 17
+var height = 162
+print ( name ,hobby1 ,hobby2 ,age ,height )
+//
+var friendhobby1 = "riding"
+var freindAge = 18
+let freindHeight = 162
+var friendname = "teeb"
+//
+var age_difference = age < freindAge
+var hieght_fifference = height == freindHeight
+
