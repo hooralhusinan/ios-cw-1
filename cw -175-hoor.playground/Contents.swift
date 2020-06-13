dc844fb167100c095d10288b8961dcff64999af6
hooralhusinan <66676227+hooralhusinan@users.noreply.github.com>
2020-06-13 19:46:38 +0300

    newwww changess

--- cw -175-hoor.playground/Contents.swift
+++ cw -175-hoor.playground/Contents.swift
@@ -8,11 +8,12 @@ var age = 17
 var height = 162
 print ( name ,hobby1 ,hobby2 ,age ,height )
 //
-var friendhobby1 = "riding"
+var friendhobby1 = "swimming"
 var freindAge = 18
 let freindHeight = 162
 var friendname = "teeb"
 //
-var age_difference = age < freindAge
+var age_difference = age - freindAge
 var hieght_fifference = height == freindHeight
-
+var nameDiffrence = name != friendname
+var hobbydiffrence = hobby1 == friendhobby1
